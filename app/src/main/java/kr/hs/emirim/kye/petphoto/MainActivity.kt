e8e76365adc16be25d8dd6bdd3d36362588a9af4
Uque1013 <kye6735@naver.com>
2023-05-23 19:34:03 +0900

    직접풀어보기 4-4 시작함 Switch로 변경, 선택완료버튼 삭제

--- app/src/main/java/kr/hs/emirim/kye/petphoto/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kye/petphoto/MainActivity.kt
@@ -9,23 +9,22 @@ import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RadioGroup
+import android.widget.Switch
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var checkStart:CheckBox
+    lateinit var checkStart:Switch
     lateinit var rg:RadioGroup
     lateinit var linear:LinearLayout
-    lateinit var btnDone:Button
     lateinit var imgv:ImageView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) 
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         checkStart = findViewById(R.id.check_start)
         rg = findViewById(R.id.rg)
         linear = findViewById(R.id.linear)
-        btnDone = findViewById(R.id.btn_done)
         imgv = findViewById(R.id.imgv)
         linear.visibility = View.INVISIBLE
 
@@ -38,12 +37,11 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        btnDone.setOnClickListener {
+        rg.setOnCheckedChangeListener{ compoundButton, b->
             when(rg.checkedRadioButtonId){
                 R.id.radio_dog -> imgv.setImageResource(R.drawable.dog)
                 R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
                 R.id.radio_rabbit -> imgv.setImageResource(R.drawable.rabit)
-                else -> Toast.makeText(applicationContext, "라디오버튼이 선택되지 않았슴둥.", Toast.LENGTH_SHORT).show()
             }
         }
     }
